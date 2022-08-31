333b90859778ad6930348098cb9081106509e4a8
Jon Dufty <30595768+jonDufty@users.noreply.github.com>
2022-08-31 21:44:33 +1000

    Add SST stack and Up API client (#3)
    * Add SST stack configuration
    
    * Add upbank lib
    
    * Generate upbank client code

---
+++ libs/upbank/README.md
@@ -0,0 +1,7 @@
+# upbank-upbank
+
+This library was generated with [Nx](https://nx.dev).
+
+## Running lint
+
+Run `nx lint upbank-upbank` to execute the lint via [ESLint](https://eslint.org/).
---
+++ stacks/index.ts
@@ -0,0 +1,8 @@
+import { App } from '@serverless-stack/resources';
+
+export default function (app: App) {
+  app.setDefaultFunctionProps({
+    runtime: 'go1.x',
+    srcPath: '../apps',
+  });
+}
