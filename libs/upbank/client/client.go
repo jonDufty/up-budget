8b0c4f01fce25036f7823a1391cbbf3b28e822ce
Jon Dufty <jonathan.dufty@99designs.com>
2022-09-06 18:29:59 +1000

    Add transaction handler code

---
+++ libs/upbank/client/client.go
@@ -0,0 +1,70 @@
+package client
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"net/http"
+	"time"
+)
+
+type UpbankClient struct {
+	client *ClientWithResponses
+	apiKey string
+}
+
+type UpbankConfig struct {
+	Endpoint string
+	ApiKey   string
+}
+
+func NewUpbankClient(cfg UpbankConfig) *UpbankClient {
+	client, err := NewClientWithResponses(cfg.Endpoint)
+	if err != nil {
+		log.Fatal("could not create client", err)
+	}
+
+	return &UpbankClient{
+		client: client,
+		apiKey: cfg.ApiKey,
+	}
+}
+
+func (c *UpbankClient) addAuthHeader(ctx context.Context, req *http.Request) error {
+	req.Header.Set("Authorization", fmt.Sprintf("Bearer %s", c.apiKey))
+	return nil
+}
+
+func (c *UpbankClient) overideUrl(ctx context.Context, req *http.Request, url string) (func(context.Context, *http.Request) error, error) {
+	newUrl, err := req.URL.Parse(url)
+	if err != nil {
+		return nil, fmt.Errorf("failed to parse link URL, %W", err)
+	}
+
+	return func(ctx context.Context, req *http.Request) error {
+		req.URL = newUrl
+		return nil
+	}, nil
+}
+
+func (c *UpbankClient) GetTransactions(ctx context.Context, pageSize int, from *time.Time, to *time.Time) ([]TransactionResource, error) {
+	var status TransactionStatusEnum = "SETTLED"
+	params := &GetTransactionsParams{
+		PageSize:     &pageSize,
+		FilterStatus: &status,
+		FilterSince:  from,
+		FilterUntil:  to,
+	}
+
+	resp, err := c.client.GetTransactionsWithResponse(ctx, params, c.addAuthHeader)
+	if err != nil {
+		return nil, fmt.Errorf("error fetching transactions, %w", err)
+	}
+
+	if resp == nil {
+		return nil, fmt.Errorf("response is empty")
+	}
+
+	transactions := resp.JSON200.Data
+	return transactions, nil
+}
