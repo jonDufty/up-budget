b625a0c61fa845db1e49ad280c36af7b4dadbf88
Jon Dufty <jonathan.dufty@99designs.com>
2022-10-03 23:42:14 +1100

    Fix node version and add debug info

--- apps/budget-app/pages/api/auth/[...nextauth].ts
+++ apps/budget-app/pages/api/auth/[...nextauth].ts
@@ -5,10 +5,11 @@ export const authOptions: NextAuthOptions = {
   // Configure one or more authentication providers
   providers: [
     GithubProvider({
-      clientId: process.env.GITHUB_CLIENT_ID!,
+      clientId: process.env.GITHUB_CLIENT_ID,
       clientSecret: process.env.GITHUB_CLIENT_SECRET,
     }),
-  ]
+  ],
+  debug: true
   // pages: {
   //   signIn: '/login'
   // }
