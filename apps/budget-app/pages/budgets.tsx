70fb25ff5b5341c8df65682f57941c3f817d9df3
Jon Dufty <jonathan.dufty@99designs.com>
2022-10-09 17:33:51 +1100

    Add budget API calls

--- apps/budget-app/pages/budgets.tsx
+++ apps/budget-app/pages/budgets.tsx
@@ -1,17 +1,43 @@
-import styled from '@emotion/styled';
+import useSWR, { Fetcher } from "swr";
+
+interface BudgetInfo {
+  category: string;
+  limit?: number;
+  id?: number
+}
+
+const API_URL = "https://api.budget-dev.jdufty.com"
+
+const budgetFetcher: Fetcher<BudgetInfo[]> = async (url: string) => {
+  const apiUrl = API_URL + url
+  const res = await fetch(apiUrl)
+  if (!res.ok) {
+    throw new Error(`Error fetching data ${res.status} ${res.statusText}`)
+  }
+  const budgets = await res.json() as BudgetInfo[]
+  return budgets
+}
 
 /* eslint-disable-next-line */
 export interface BudgetsProps {}
 
-const StyledBudgets = styled.div`
-  color: pink;
-`;
-
 export function Budgets(props: BudgetsProps) {
+  const { data: budgets, error } = useSWR("/budgets", budgetFetcher);
+
+  if (error) {
+    console.error(error)
+    return <h1>An error has occurred</h1>
+  }
+  if (!budgets) return <h4>Loading...;</h4>
+
   return (
-    <StyledBudgets>
-      <h1>Welcome to Budgets!</h1>
-    </StyledBudgets>
+    <div>
+      {
+        budgets.map((b, i) => {
+          return <div key={`${b.category}-${i}`}>{b.category}, {b.limit}</div>
+        })
+      }
+    </div>
   );
 }
 
--- apps/budget-app/pages/merchants.tsx
+++ apps/budget-app/pages/merchants.tsx
@@ -1,6 +1,36 @@
 import styled from '@emotion/styled';
 import { CategoryBar, MerchantInfo } from '@up-budget/ui';
 import { GetServerSideProps } from 'next';
+import useSWR, { Fetcher } from 'swr';
+
+const API_URL = "https://api.budget-dev.jdufty.com"
+
+const fetcher: Fetcher<MerchantInfo[]> = async (url: string) => {
+  const apiUrl = API_URL + url
+  const res = await fetch(apiUrl)
+  if (!res.ok) {
+    throw new Error(`Error fetching data ${res.status} ${res.statusText}`)
+  }
+  const merchants = await res.json() as MerchantInfo[]
+  return merchants
+};
+
+interface BudgetInfo {
+  category: string;
+  limit?: number;
+  id?: number
+}
+
+const budgetFetcher: Fetcher<BudgetInfo[]> = async (url: string) => {
+  const apiUrl = API_URL + url
+  const res = await fetch(apiUrl)
+  if (!res.ok) {
+    throw new Error(`Error fetching data ${res.status} ${res.statusText}`)
+  }
+  const budgets = await res.json() as BudgetInfo[]
+  return budgets
+}
+
 
 const MERCHANTS: MerchantInfo[] = [
   {
@@ -38,24 +68,27 @@ const CATEGORIES = [
 
 /* eslint-disable-next-line */
 export interface MerchantsProps {
-  merchants: MerchantInfo[]
-  categories: string[]
+
 }
 
-export function Merchants({merchants, categories}: MerchantsProps) {
+export function Merchants(props) {
+  const { data: merchants, error } = useSWR("/merchants", fetcher);
+  const { data: budgets, error: errorBudgets } = useSWR("/budgets", budgetFetcher);
+
+  if (error || errorBudgets) {
+    console.error(error, errorBudgets)
+    return <h1>An error has occurred</h1>
+  }
+  if (!merchants) return <h4>Loading...;</h4>
+
+  let categories: string[]
+  if (budgets) {
+    categories = budgets.map((m) => m.category)
+  }
+
   return (
     <CategoryBar merchants={merchants} categories={categories} />
   );
 }
 
 export default Merchants;
-
-export const getServerSideProps: GetServerSideProps = async (context) => {
-
-  return {
-    props: {
-      merchants: MERCHANTS,
-      categories: CATEGORIES
-    }
-  }
-}
