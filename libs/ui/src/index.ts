c87c4c4a46026db9a7dfacd78bbe5f8010656b46
Jon Dufty <jonathan.dufty@99designs.com>
2022-10-09 17:56:41 +1100

    Add BudgetMenu component

--- libs/ui/src/index.ts
+++ libs/ui/src/index.ts
@@ -1,6 +1,6 @@
+export * from './lib/BudgetMenu/BudgetMenu';
 export * from './lib/CategoryBar/CategoryBar';
 export * from './lib/NavBar/NavBar';
 export * from './lib/Menu/Menu';
 export * from './lib/Menu/MenuItem';
-export * from './lib/Ui';
 export * from './theme/DefaultTheme';
