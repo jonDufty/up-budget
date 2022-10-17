4ea3b698040f476b3281160ee6abf921d9f2df89
Jon Dufty <jonathan.dufty@99designs.com>
2022-10-18 08:54:01 +1100

    Add basic graph

--- apps/budget-app/pages/dashboard.tsx
+++ apps/budget-app/pages/dashboard.tsx
@@ -2,13 +2,36 @@ import styled from '@emotion/styled';
 import { useSession } from 'next-auth/react';
 import Image from 'next/image';
 import { GithubLoginButton } from '../components/AppMenu/GithubLoginButton';
-
+import { MonthlyGraph } from '@up-budget/ui';
 /* eslint-disable-next-line */
 export interface DashboardProps {}
 
 const StyledDashboard = styled.div`
   color: pink;
 `;
+
+const chartData = [
+  {
+    category: "rent",
+    amount: 2000,
+    limit: 2000
+  },
+  {
+    category: "utilities",
+    amount: 600,
+    limit: 500
+  },
+  {
+    category: "groceries",
+    amount: 500,
+    limit: 700
+  },
+  {
+    category: "other",
+    amount: 200,
+    limit: 1000
+  }
+]
 
 export function Dashboard(props: DashboardProps) {
   const { data: session} = useSession()
@@ -19,6 +42,7 @@ export function Dashboard(props: DashboardProps) {
       {session &&
         <Image src={session.user.image} alt="No image" width={200} height={200} />
       }
+      <MonthlyGraph chartData={chartData} />
     </StyledDashboard>
   );
 }
---
+++ libs/ui/src/lib/MonthlyGraph/MonthlyGraph.stories.tsx
@@ -0,0 +1,38 @@
+import { ComponentStory, ComponentMeta } from '@storybook/react';
+import { MonthlyGraph } from './MonthlyGraph';
+
+const Story: ComponentMeta<typeof MonthlyGraph> = {
+  component: MonthlyGraph,
+  title: 'MonthlyGraph',
+};
+export default Story;
+
+const Template: ComponentStory<typeof MonthlyGraph> = (args) => <MonthlyGraph {...args} />;
+
+export const Primary = Template.bind({});
+
+
+Primary.args = {
+  chartData: [
+    {
+      category: "rent",
+      amount: 2000,
+      limit: 2000
+    },
+    {
+      category: "utilities",
+      amount: 600,
+      limit: 500
+    },
+    {
+      category: "groceries",
+      amount: 500,
+      limit: 700
+    },
+    {
+      category: "other",
+      amount: 200,
+      limit: 1000
+    }
+  ]
+};
