0feefbddcfa2ee7c61eb85e74332cc998ef5fa36
Jon Dufty <jonathan.dufty@99designs.com>
2022-10-23 19:20:39 +1100

    Add generic fetcher/mutation functions

---
+++ libs/frontend-api-client/src/lib/fetcher.ts
@@ -0,0 +1,15 @@
+import { Fetcher } from "swr";
+
+import {API_BASE_URL} from './api'
+
+type PayloadType = Record<string, any> | any[]
+
+export async function budgetFetcher<T extends PayloadType>(url: string) {
+  const apiUrl = API_BASE_URL + url;
+  const res = await fetch(apiUrl);
+  if (!res.ok) {
+    throw new Error(`Error fetching data ${res.status} ${res.statusText}`);
+  }
+  const data = (await res.json()) as T;
+  return data;
+};
