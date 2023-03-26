d73af7fa77303d7827aae5965cfae9dec05a766d
Jon Dufty <jonathan.dufty@99designs.com>
2023-03-26 12:46:22 +1100

    Add selected state styling to button

--- libs/ui/src/lib/Button/Button.stories.tsx
+++ libs/ui/src/lib/Button/Button.stories.tsx
@@ -1,6 +1,7 @@
+import { Typography } from '@mui/material';
 import { Stack } from '@mui/system';
 import { ComponentStory, ComponentMeta } from '@storybook/react';
-import { Button } from './Button';
+import { Button, ButtonToggle, ButtonProps } from './Button';
 
 const Story: ComponentMeta<typeof Button> = {
   component: Button,
@@ -9,13 +10,13 @@ const Story: ComponentMeta<typeof Button> = {
 export default Story;
 
 const Template: ComponentStory<typeof Button> = (args) => (
-<Stack spacing={2} direction="column">
-    <Button appearance='primary'>Primary</Button>
+  <Stack spacing={2} direction="column">
+    <Button appearance="primary">Primary</Button>
     <Button appearance="secondary">Secondary</Button>
     <Button appearance="error">Error</Button>
     <Button appearance="transparent">Transparent</Button>
   </Stack>
-  );
+);
 
 // A story rendering the button as normal
 export const Primary = Template.bind({});
@@ -30,3 +31,24 @@ const SizesStory: ComponentStory<typeof Button> = (args) => (
 );
 
 export const Sizes = SizesStory.bind({});
+const appearances: ButtonProps['appearance'][] = ['primary', 'secondary', 'error', 'transparent'];
+const StatesStory: ComponentStory<typeof Button> = (args) => (
+  <Stack spacing={2} direction="column">
+    {appearances.map((appearance) => (
+      <>
+        <Typography>{appearance}</Typography>
+        <Stack spacing={2} direction="row">
+          <Button appearance={appearance}>Active</Button>
+          <Button appearance={appearance} disabled>
+            Disabled
+          </Button>
+          <ButtonToggle value={'selected'} selected appearance={appearance}>
+            Selected
+          </ButtonToggle>
+        </Stack>
+      </>
+    ))}
+  </Stack>
+);
+
+export const States = StatesStory.bind({});
